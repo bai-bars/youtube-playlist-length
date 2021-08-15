ced142dfd2c9e38dcf34dfc8f8e19b51a4a68ad6
bai-bars <sazin.me@gmail.com>
2021-08-15 19:20:04 +0600

    .

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ def send_css(filename):
     return static_file(filename, root='static/css')
 
 if os.environ.get('APP_LOCATION') == 'heroku':
-    run()
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
     run(host='localhost', port=8080, debug=True)
